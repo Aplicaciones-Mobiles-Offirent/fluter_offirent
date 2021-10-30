5062c095dc4f95ec93f6d24e521c6514d76c8629
FloresIrene <71415300+FloresIrene@users.noreply.github.com>
2021-10-30 06:23:41 -0500

    feat: added office detail

---
+++ lib/office_detail.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class OfficeDetail extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Información de la oficina",
+              style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.tealAccent,
+        ),
+        body: ListView(
+          children: <Widget>[
+            Container(
+              child: const Text("OFICINA",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                ),
+              ),
+              margin: EdgeInsets.all(10.0),
+            ),
+            Container(
+              child: Image.network("https://cdn.pixabay.com/photo/2015/05/15/14/22/conference-room-768441_960_720.jpg",
+                  fit: BoxFit.cover),
+              margin: EdgeInsets.all(10.0),
+            ),
+            Container(
+              child: const Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              margin: EdgeInsets.all(10.0),
+            ),
+            Container(
+              child: const Text("Correo: offi@hotmail.com",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              margin: EdgeInsets.all(10.0),
+            ),
+            Container(
+              child: const Text("Telefono: +51 985 265 412",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              margin: EdgeInsets.all(10.0),
+            ),
+            Container(
+              child: MaterialButton(
+                  color: Colors.tealAccent,
+                  child: const Text("Alquilar local S/.390",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),),
+                  onPressed: (){}),
+              margin: EdgeInsets.all(10.0),
+            ),
+            TextButton.icon(
+                icon: Icon(Icons.favorite),
+                label: const Text("Agregar a favoritos",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),),
+                onPressed: (){}),
+          ],
+        ),
+      ),
+    );
+  }
+}
